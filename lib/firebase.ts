fdfc0803611ce4c65b740a9693b46c0e32aee9bf
Ali-Tamerr <extelx66@gmail.com>
2026-05-16 23:09:08 +0300

    Refactor the configuration to use process.env variables.

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBrWY_9oRHxq2nYXXwVLOEVQw93YVeBofc",
-  authDomain: "ai-saas-landing-page-auth-95.firebaseapp.com",
-  projectId: "ai-saas-landing-page-auth-95",
-  storageBucket: "ai-saas-landing-page-auth-95.firebasestorage.app",
-  messagingSenderId: "258719850205",
-  appId: "1:258719850205:web:2b2046719a9ffe069f4534"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
