9ff6599a07918d18356a055caabf776748c86134
Ali-Tamerr <extelx66@gmail.com>
2026-06-05 22:33:54 +0300

    fix all build errors

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -6,7 +6,7 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   asChild?: boolean
   variant?: "default" | "outline" | "ghost"
-  size?: "default" | "sm" | "lg"
+  size?: "default" | "sm" | "lg" | "xs"
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
@@ -24,6 +24,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
             "h-10 px-4 py-2": size === "default",
             "h-9 px-3": size === "sm",
             "h-12 px-8 text-base": size === "lg",
+            "h-7 px-2.5 text-[11px]": size === "xs",
           },
           className
         )}
